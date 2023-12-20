d8eff3d906f0d327553beadafbf3bae68aea66b8
Javier Masis <jmasisdev@gmail.com>
2023-12-20 14:55:45 -0600

    sticky header

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,3 +55,16 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
+// Sticky header
+const primaryHeader = document.querySelector('.primary-header');
+const scrollWatcher = document.createElement('div');
+scrollWatcher.setAttribute('data-scroll-watcher', '');
+primaryHeader.before(scrollWatcher);
+
+const navObserver = new IntersectionObserver((entries) => {
+  primaryHeader.classList.toggle('sticking', !entries[0].isIntersecting)
+}, {
+  rootMargin: '0px 0px 0px 0px'
+});
+
+navObserver.observe(scrollWatcher);
